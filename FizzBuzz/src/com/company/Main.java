798e797704cb888b8b3dfd5a1183ff85d3a8cb16
Nicolin Dora <nicolin.dora@students.bfh.ch>
2022-04-16 18:43:09 +0200

    FizzBuzz Project added

---
+++ FizzBuzz/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                System.out.println("FizzBuzz");
+            } else if ((i % 3 == 0)) {
+                System.out.println("Fizz");
+            } else if ((i % 5 == 0)) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
