57b9c2439b8efe51315880c3942ea2b4b23c0201
Nicolin Dora <nicolin.dora@students.bfh.ch>
2022-04-19 21:09:44 +0200

    Changed BinarySearch to search in a 2nd Dimensional Array... Not that complicated

--- sandbox/src/com/company/BinarySearch.java
+++ sandbox/src/com/company/BinarySearch.java
@@ -2,16 +2,16 @@ package com.company;
 
 public class BinarySearch {
     // return index of x if it is present in arr[l, r], else return -1
-    int binarySearch(int arr[], int left, int right, int x){
+    int binarySearch(int arr[][], int left, int right, int x){
         if(right >= left){
             int mid = left + (right-left) / 2;
 
             // If the element is present at the middle itself
-            if(arr[mid] == x){
+            if(arr[mid][1] == x){
                 return mid;
             }
             // If element is smaller than mid, then it can only be present in left subarray
-            if(arr[mid] > x){
+            if(arr[mid][1] > x){
                 return binarySearch(arr, left, mid -1, x);
             }
             // Lese the element can only be present in right subarray
